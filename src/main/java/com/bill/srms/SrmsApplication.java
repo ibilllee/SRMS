22b0d66f78be2e16421bc3397e928d087967f706
ibilllee <ibilllee@qq.com>
2021-06-06 11:17:55 +0800

    Init

---
+++ src/main/java/com/bill/srms/SrmsApplication.java
@@ -0,0 +1,14 @@
+package com.bill.srms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrmsApplication
+{
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bill/srms/SrmsApplicationTests.java
@@ -0,0 +1,14 @@
+package com.bill.srms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrmsApplicationTests
+{
+
+	@Test
+	void contextLoads() {
+	}
+
+}
