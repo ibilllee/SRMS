10875f6b87bc71e9462c615210885ea40ede2a99
ibilllee <ibilllee@qq.com>
2021-06-06 11:36:56 +0800

    Init

--- src/main/java/com/bill/srms/SrmsApplication.java
+++ src/main/java/com/bill/srms/SrmsApplication.java
@@ -2,8 +2,10 @@ package com.bill.srms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+@MapperScan("com.bill.srms.mapper")
 public class SrmsApplication
 {
 
